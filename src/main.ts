337c06f1c5a9ef0c40d6e69f3e87f4cbc49f2b2b
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2022-07-18 15:54:33 +0200

    Add qwerty board for visual reference

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 import homePage from "./templates/pages/homePage.html?raw"
+import qwertyBoard from "./templates/components/qwertyBoard.html?raw"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = homePage;
 document.querySelector<HTMLImageElement>('#vanillaTSLogo')!.src = typescriptLogo
+document.querySelector<HTMLDivElement>('#qwerty-board')!.innerHTML = qwertyBoard
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
