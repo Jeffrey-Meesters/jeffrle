eed4ed62568c913e8bb33ab7854aab157b04bd29
Jeffrey <jjeffreymeesters@gmail.com>
2022-08-29 09:46:36 +0200

    Add rollup obfuscator

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import obfuscatorPlugin from 'rollup-plugin-javascript-obfuscator'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '', // now we get relative paths instead of absolute paths
+    build: {
+        rollupOptions: {
+            output: {
+                plugins: [
+                    obfuscatorPlugin({ compact: true })
+                ]
+            }
+        }
+    }
 })
